03b3244a6716db8242fe3a9dfbd2fc7906998ede
Julien Bosco <julien.bosco@cegepsherbrooke.qc.ca>
2021-05-12 12:50:37 -0400

    Changement des chemins en absolu

--- Scripts/LabDeploy.ps1
+++ Scripts/LabDeploy.ps1
@@ -6,7 +6,7 @@ catch { # Lab n'existe pas
 }
 
 # Récupération des définitions DSC
-Get-ChildItem -Path "../MOF/*" -Include *-DEV.mof | ForEach-Object {
+Get-ChildItem -Path "C:\DSC\MOF\*" -Include *-DEV.mof | ForEach-Object {
     try {
         Get-LabVM -ComputerName $_.Name
     } catch {
@@ -25,14 +25,14 @@ Install-Lab
 
 #Transfert des fichiers DSC vers les VMs
 ForEach ($vm in Get-LabVM) {
-    Copy-LabFileItem -Path "\\LAB01\DSC\MOF\$vm.Name.mof" -ComputerName $vm.Name -DestinationFolder "C:\DSC"
+    Copy-LabFileItem -Path "C:\DSC\MOF\$vm.Name.mof" -ComputerName $vm.Name -DestinationFolder "C:\DSC"
 }
 
 Invoke-LabCommand -ComputerName PUSH01-DEV -ScriptBlock {
     New-Item -Path "C:\MODULES" -ItemType Directory
     New-SmbShare -Name MODULES -Path "C:\MODULES" -ReadAccess Everyone
 }
-Copy-LabFileItem -Path "\\LAB01\DSC\CModules\*" -ComputerName PUSH01-DEV -DestinationFolder "C:\MODULES"
+Copy-LabFileItem -Path "C:\DSC\CModules\*" -ComputerName PUSH01-DEV -DestinationFolder "C:\MODULES"
 Invoke-LabCommand -ComputerName DC01-DEV -ScriptBlock { Start-DscConfiguration -Path "C:\DSC"}
 Show-LabDeploymentSummary
 
